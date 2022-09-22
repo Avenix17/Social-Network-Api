4c8a164295da6852346c068e851edb34045588da
Avyrie Fellows <avyriefellows@gmail.com>
2022-09-22 11:50:42 -0600

    Created models users, thoughts, and index

---
+++ models/thoughts.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timeStamp) => timeStamp.toLocaleString()
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timeStamp) => timeStamp.toLocaleString()
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function() {
+
+    });
+
+const Thoughts = model('Thoughts', thoughtSchema);
+
+module.exports = Thoughts;
